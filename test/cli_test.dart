b13312912693b55b766260d26a6eae8d7a423f17
valentin.doering <valentinmdoering@gmail.com>
2021-11-10 15:16:29 +0100

    correction

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -24,6 +24,6 @@ void main() {
     var grid2 = Grid.from(gridWidth, 0);
     var doubleGrid = grid + grid2;
     expect(doubleGrid.width, gridWidth * 2);
-    expect(false, true);
+    expect(true, true);
   });
 }
